90023306d33662e845902c6d455f3cb3c86e5226
Deve Tambat <54927248+tambatd@users.noreply.github.com>
2021-04-22 18:48:02 -0400

    Update sketch_210407a.js

--- sketch_210407a.js
+++ sketch_210407a.js
@@ -10,17 +10,22 @@ function setup() {
     //background(50);
     frameRate(30);
   createCanvas(window.innerWidth, window.innerHeight);
-  song = loadSound('brodyquest.mp3');
 
-  image(img, window.innerWidth/2, window.innerHeight/2-150, 100, 100);
+  image(img, window.innerWidth/2-50, window.innerHeight/2-150, 100, 100);
   stroke(255);
   fill(255);
+       song = loadSound('brodyquest.mp3');
+       textAlign(CENTER);
+
+button = createButton('possum on da roomba fo today');
+  button.position(window.innerWidth/2-100, window.innerHeight/2+100);
+  button.mousePressed(audio);
 textSize(15);
 textAlign(CENTER);
 fill(255);
 textSize(10);
 //text('BRODYQUEST by Neil Cicierega', 80, 25);
-text('possum on da roomba fo today', window.innerWidth/2, window.innerHeight/2);
+//text('possum on da roomba fo today', window.innerWidth/2, window.innerHeight/2);
 }
 //brodyquest
 
@@ -31,11 +36,9 @@ let s = random(500);
   image(img, random(window.innerWidth), random(window.innerHeight),s, s);
   }
 }
+function audio() {
+    song.play();
+    x= true;
+    button.remove();
+}
 
-function touchStarted(){
-  if(x==false){
-  song.play();
-}
-  console.log(x);
-  x= true;
-}
