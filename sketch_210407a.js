6b6f8bba9da4ff655baf096de37f3e828b44391c
Deve Tambat <54927248+tambatd@users.noreply.github.com>
2021-04-22 18:17:03 -0400

    Add files via upload

--- sketch_210407a.js
+++ sketch_210407a.js
@@ -1,23 +1,28 @@
 var img;
+var song;
 var x = false;
 function preload() {
   img = loadImage('tenor.gif');
 }
 
 function setup() {
+    
     //background(50);
     frameRate(30);
   createCanvas(window.innerWidth, window.innerHeight);
+  song = loadSound('brodyquest.mp3');
 
   image(img, window.innerWidth/2, window.innerHeight/2-150, 100, 100);
   stroke(255);
   fill(255);
 textSize(15);
 textAlign(CENTER);
-
+fill(255);
+textSize(10);
+//text('BRODYQUEST by Neil Cicierega', 80, 25);
 text('possum on da roomba fo today', window.innerWidth/2, window.innerHeight/2);
 }
-
+//brodyquest
 
 function draw() {
   if(x==true)
@@ -28,6 +33,9 @@ let s = random(500);
 }
 
 function touchStarted(){
+  if(x==false){
+  song.play();
+}
   console.log(x);
   x= true;
 }
